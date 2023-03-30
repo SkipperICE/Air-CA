85f2cea0cd866c573e1e422abb200d2226a9cfbe
SkipperICE <121302022+SkipperICE@users.noreply.github.com>
2023-03-30 11:51:52 +0200

    Air01

--- Air01.py
+++ Air01.py
@@ -1,19 +1,19 @@
 import sys
 
-def ma_fonction(string_a_couper, string_separateur):
+def ma_fonction(string_a_couper, string_separateur=None):
     try:
-        tableau = string_a_couper.split(string_separateur)
+        tableau = string_a_couper.split()
         return tableau
     except AttributeError:
         print("Erreur : veuillez fournir une chaîne de caractères en tant que premier argument.")
         sys.exit(1)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Erreur : veuillez fournir deux arguments : une chaîne de caractères à couper et un séparateur.")
+    if len(sys.argv) != 2:
+        print("Erreur : veuillez fournir 1 arguments : une chaîne de caractères")
         sys.exit(1)
     else:
         string_a_couper = sys.argv[1]
-        string_separateur = sys.argv[2]
+        string_separateur = None
         tableau = ma_fonction(string_a_couper, string_separateur)
         print("\n".join(tableau))
